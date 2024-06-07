834991f9153057b78c5467e0c32e33880e13f15c
Jay-A-Kad <jaykad982@gmail.com>
2024-06-07 19:44:07 +0530

    added player sprite flip

--- main.py
+++ main.py
@@ -15,17 +15,46 @@ PLAYER_VEL = 5
 # display window
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# making a player
+
+# flip a sprite
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=True):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+    for image in images:
+        sprite_sheets = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheets.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width,  height)
+            surface.blit(sprite_sheets, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+
+    return all_sprites
+    # making a player
 
 
 class Player(pygame.sprite.Sprite):
     COLOR = (0, 255, 0)
+    SPRITES = load_sprite_sheets("MainCharacters", "NinjaFrog", 32, 32, True)
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0
         self.y_vel = 0
-        self.direction = 'Left'
+        self.direction = 'left'
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -45,7 +74,8 @@ class Player(pygame.sprite.Sprite):
         self.move(self.x_vel, self.y_vel)
 
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, self.rect)
+        self.sprite = self.SPRITES["idle_" + self.direction][0]
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 # handle player movement
